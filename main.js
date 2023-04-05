513926ef07aee180a0bcb49ada766a68c9de13d0
Bruno Canini <brunocanini1994@gmail.com>
2023-04-05 16:23:32 +0200

    prova fine

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let nomeUtente = prompt("nome utente?");
+document.getElementById("nome").innerText = (nomeUtente);
 
 let cognomeUtente = prompt("cognome utente?");
+document.getElementById("cognome").innerText = (cognomeUtente);
 
 let colorePreferito = prompt("colore preferito?");
+document.getElementById("colore").innerText = (colorePreferito);
 
 let annoCorrente = 2023
 
 let generatorePasword = (`${nomeUtente}-${cognomeUtente}-${colorePreferito}-${annoCorrente}`);
-
+document.getElementById("pasword").innerText = (generatorePasword);
 
