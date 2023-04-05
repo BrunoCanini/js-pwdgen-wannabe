8e34b3a9ca82a8e1f86cf229fd04623fb78996ec
Bruno Canini <brunocanini1994@gmail.com>
2023-04-05 16:54:46 +0200

    data

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ document.getElementById("cognome").innerText = (cognomeUtente);
 const colorePreferito = prompt("colore preferito?");
 document.getElementById("colore").innerText = (colorePreferito);
 
-const annoCorrente = 2023
+// const annoCorrente = 2023
+
+const annoCorrente = new Date();
 
 const generatorePasword = (`${nomeUtente}-${cognomeUtente}-${colorePreferito}-${annoCorrente}`);
 document.getElementById("pasword").innerText = (generatorePasword);
