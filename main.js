a8a420fcf08638469b60a626194741351a5edbe3
Bruno Canini <brunocanini1994@gmail.com>
2023-04-05 16:20:09 +0200

    prova

---
+++ main.js
@@ -0,0 +1,11 @@
+let nomeUtente = prompt("nome utente?");
+
+let cognomeUtente = prompt("cognome utente?");
+
+let colorePreferito = prompt("colore preferito?");
+
+let annoCorrente = 2023
+
+let generatorePasword = (`${nomeUtente}-${cognomeUtente}-${colorePreferito}-${annoCorrente}`);
+
+
